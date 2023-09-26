21a7574528bf6d2f0e3df32c1e8311e0eacbc14a
Abid Ali KP <apathikkal@mvement.co>
2023-09-26 23:04:30 +0530

    chakingal header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,10 @@ import D3Tree from "./D3-Tree";
 
 const Main = () => {
   return (
-    <div className="w-full h-screen">
+    <div className="flex flex-col h-screen">
+      <div className="w-full p-2 text-xl font-bold text-center text-white bg-black line-clamp-1">
+        CHAKINGAL
+      </div>
       <D3Tree />
     </div>
   );
